febb19f81d670378945ec4f6bb346ccf78de33fb
邹昊 <1062156015@qq.com>
2017-11-20 09:57:48 +0800

    ...

--- program_experiment/main.cpp
+++ program_experiment/main.cpp
@@ -81,88 +81,88 @@ public:
     
 };
 
-void init_connect() {
-    MySQLManager *mysql = new MySQLManager("127.0.0.1", "root", "password", "mysql", (unsigned int)3306);
+int main()
+{
+    MySQLManager *mysql = new MySQLManager("123.207.183.132", "root", "rNc6NKKWWL", "mysql", (unsigned int)3306);
     mysql->initConnection();
     if(mysql->getConnectionStatus())
         
     {
-        if(mysql->runSQLCommand("show databases"))
-        {
-            vector<vector<std::string> > result = mysql->getResult();
-            for(auto & vec : result)
-            {
-                for(auto &str : vec) {
-                    cout << str.c_str() << " ";
+        cout << "连接成功" << endl;
+        
+        char username;
+        char password;
+        string method;
+        time_t t = time(0);
+        struct tm *p;
+        p=gmtime(&t);
+        char s[100];
+        strftime(s, sizeof(s), "%Y-%m-%d %H:%M:%S", p);
+        printf("%ld: %s\n", (long)t, s);
+        
+        
+        if(mysql->runSQLCommand("select * from parking.admin")) {
+            cout << "Please input your username: ";
+            cin >> username;
+            cout << "Please input your password: ";
+            cin >> password;
+            
+            vector<vector<std::string>> result = mysql->getResult();
+            for(auto & vec : result) {
+                for (auto &str : vec) {
+                    if ()
                 }
-                cout << endl;
+            }
+        };
+    
+        
+        if (password == 666666) {
+            for (int k = 0; k < 100; k++) {
+                
+                cout << "Please input entry or claer" << endl;
+                cin >> method;
+                if (method == "entry") {
+                    double left[10] = {1,2,3,4,5,6,7,8,9,10};
+                    cout << "剩余车位 ：";
+                    for (int i = 0; i < 10; i++) {
+                        if (i == 9) {
+                            cout << left[i] << endl;
+                        }
+                        else
+                            cout << left[i] << ",";
+                    }
+                    
+                    Car car;
+                    car.get_id();
+                    car.get_time(t, s);
+                    car.get_number();
+                    car.get_model();
+                    car.change_state();
+                    
+                }
+                
+                else if (method == "clear") {
+                    time_t entry_time = 1510630398;
+                    
+                    Parting space;
+                    double last_time = space.get_time(t, s);
+                    double time_difference = last_time - entry_time;
+                    double total = (time_difference/60) * space.rate;
+                    
+                    space.state = true;
+                    space.change_state();
+                    cout << "Total:" << total << endl;
+                }
+                
+                else
+                    cout << "Error" << endl;
             }
         }
-        else
-            cout << "执行失败" << endl;
+        else {
+            cout << "Please input the true password";
+        }
+        
     }
     else
         cout << "连接未建立" << endl;
-}
-
-int main()
-{
-    init_connect();
-    
-    double password;
-    string method;
-    time_t t = time(0);
-    struct tm *p;
-    p=gmtime(&t);
-    char s[100];
-    strftime(s, sizeof(s), "%Y-%m-%d %H:%M:%S", p);
-    printf("%ld: %s\n", (long)t, s);
-    
-    cout << "Please input your password: ";
-    cin >> password;
-    if (password == 666666) {
-        for (int k = 0; k < 100; k++) {
-            
-            cout << "Please input entry or claer" << endl;
-            cin >> method;
-            if (method == "entry") {
-                double left[10] = {1,2,3,4,5,6,7,8,9,10};
-                cout << "剩余车位 ：";
-                for (int i = 0; i < 10; i++) {
-                    if (i == 9) {
-                        cout << left[i] << endl;
-                    }
-                    else
-                        cout << left[i] << ",";
-                }
-                
-                Car car;
-                car.get_id();
-                car.get_time(t, s);
-                car.get_number();
-                car.get_model();
-                car.change_state();
-                
-            }
-            
-            else if (method == "clear") {
-                time_t entry_time = 1510630398;
-                
-                Parting space;
-                double last_time = space.get_time(t, s);
-                double time_difference = last_time - entry_time;
-                double total = (time_difference/60) * space.rate;
-                
-                space.state = true;
-                space.change_state();
-                cout << "Total:" << total << endl;
-            }
-            
-            else
-                cout << "Error" << endl;
-        }
-    }
-    else {
-        cout << "Please input the true password";
-    }
 }
